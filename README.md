f1ba041ae736d0f9611ae1aef97bfed3525dc305
Arthur Guedes <arthurguedes375@gmail.com>
2020-08-09 21:42:42 -0300

    Project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Chat Frontend made in react
+
+![Project demo](readme/readme.gif)
+
+---
+
+## Getting started
+
+- Clone the [Chat Backend](https://github.com/arthurguedes375/Chat-Node-Backend) and do that chat backend getting started
+- After clone this repository
+
+- Run this commands in the repository folder
+```
+yarn && yarn start
+```
+
+
+<p align="center">Made with :heart: <strong>Arthur Guedes</strong></p>
---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+
+import io from 'socket.io-client';
+
+
+class App extends Component {
+
+    state = {
+        username: '',
+        message: '',
+
+        messages: [],
+        socket: null
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.startConnection = this.startConnection.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+    }
+
+
+    async sendMessage(e) {
+        e.preventDefault();
+
+        let { socket, messages } = this.state;
+
+        const data = {
+            author: this.state.username,
+            message: this.state.message
+        };
+
+        if (data.author && data.message) {
+            await socket.emit("sendMessage", data);
+            if (messages.length > 0) {
+                messages.push(data);
+            } else {
+                messages = [];
+                messages.push(data);
+            }
+            this.setState({ messages, message: '' });
+        }
+    }
+
+    async startConnection() {
+        let socket = io('http://localhost:5000');
+
+        socket.on("previousMessages", (data) => {
+            this.setState({ messages: data });
+        });
+
+        socket.on("receivedMessage", (data) => {
+            let { messages } = this.state;
+            messages.push(data);
+            this.setState({ messages });
+        });
+
+        await this.setState({ socket });
+    }
+
+    componentDidMount() {
+        this.startConnection();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form id="chat" onSubmit={e => this.sendMessage(e)}>
+                    <input type="text" name="username"
+                        placeholder="Digite seu Nome..."
+                        autoComplete="off"
+                        value={this.state.username}
+                        onChange={(e) => this.setState({ username: e.target.value })}
+                    />
+                    <div className="messages">
+                        {(this.state.messages.length > 0) ? this.state.messages.map((message) => <div className="message"><strong>{message.author}</strong>: {message.message}</div>) : null}
+                    </div>
+
+                    <div className="sub">
+                        <input type="text" name="message"
+                            placeholder="Digite sua Mensagem..."
+                            autoComplete="off"
+                            value={this.state.message}
+                            onChange={(e) => this.setState({ message: e.target.value })}
+                        />
+                        <button type="submit">Enviar</button>
+                    </div>
+
+
+
+                </form>
+            </div>
+        );
+    }
+}
+
+export default App;
